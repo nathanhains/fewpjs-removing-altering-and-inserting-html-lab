716144002e4ae04731c7c12086ce0a7bea8a0f04
nathan hains <nathanhains14@yahoo.com>
2021-05-04 16:27:12 -0400

    commit tree 348c321e9533547b22b842571cf71772fb03accd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "NATHAN is the champion"
+document.appendChild(newHeader);
+
+
